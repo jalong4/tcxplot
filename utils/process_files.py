56cd87f9bd45410f7bb1fcb78d72c067e03c5b2e
Jim Long <jimlongja@google.com>
2023-04-04 12:38:56 -0700

    Make quotes consistent.

--- utils/process_files.py
+++ utils/process_files.py
@@ -56,14 +56,13 @@ def process_files(folder_path, output_dir, google_maps_api_key, launch_browser,
     df['device'] = file_name
     dfs.append(df)
 
+  sport = 'Unknown'
   if len(sports) == 1:
-      sport = sports.pop()
+    sport = sports.pop()
   elif len(sports) >= 2:
-      if "Other" in sports:
-          sports.remove("Other")
-      sport = sports.pop()
-  elif len(sports) == 0:
-      sport = "Unknown"
+    if 'Other' in sports:
+      sports.remove('Other')
+    sport = sports.pop()
 
   if start_times:
     start_time = start_times.pop()
